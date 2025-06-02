e3dc09ef197a611b191bf626cf301f9fd5b441c5
SimonMck789 <S9406133@student.rmit.edu.au>
2025-06-03 01:46:23 +1000

    Refactor post card component and update comment display for better clarity.

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -5,16 +5,21 @@
 <div class="post-card {{ $highlightClass }}">
     <div class="post-created">{{ $post->created_at->format('j F Y') }}</div>
     
-    <a class="post-title" href="{{ route('posts.show', $post->id) }}">
-        {{ $post->title }}
-    </a>
-    
-    <div class="post-tags"> 
-        @foreach ($post->tags as $tag)
+    <div class="post-main">
+        <a class="post-title" href="{{ route('posts.show', $post->id) }}">
+            {{ $post->title }}
+        </a>
+        
+        <div class="post-tags"> 
+            @foreach ($post->tags as $tag)
             <span>{{ $tag->name }}</span>
-        @endforeach
+            @endforeach
+        </div>
+        
+        <div class="post-name">{{ $post->user->name }}</div>
+        <div class="post-comments">
+            <img src="{{ asset('images/comment_icon.svg') }}" alt=""> 
+            {{ $post->comments_count }}
+        </div>
     </div>
-    
-    <div class="post-name">{{ $post->user->name }}</div>
-    <div class="post-comments">Comments {{ $post->comments_count }}</div>
 </div>
