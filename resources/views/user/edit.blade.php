8c7467572bd7ea57acf8b68e133680fd450a62f0
SimonMck789 <S9406133@student.rmit.edu.au>
2025-06-05 23:55:50 +1000

    Added profile edit toggle buttons and functionality

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -17,8 +17,8 @@
             <div class="input-cont">
                 @error('name') <span style="color:crimson">{{ $message }}</span> @enderror
                 <div class="edit-input">
-                    <input type="text" name="name" id="name" value="{{ $user->name }}" required disabled>
-                    <button type="submit" class="edit-btn" id="edit-name-btn">
+                    <input type="text" name="name" id="name" value="{{ $user->name }}" required readonly>
+                    <button type="submit" class="edit-btn" id="edit-name-btn" data-field="name" title="Edit Name">
                         <img height="24" src="{{ asset('images/edit_square.svg') }}" alt="Edit Info">
                     </button>
                 </div>
@@ -28,8 +28,8 @@
             <div class="input-cont">
                 @error('email') <span style="color:crimson">{{ $message }}</span> @enderror
                 <div class="edit-input">
-                    <input type="email" name="email" id="email" value="{{ $user->email }}" required disabled>
-                    <button type="submit" class="edit-btn" id="edit-email-btn">
+                    <input type="email" name="email" id="email" value="{{ $user->email }}" required readonly>
+                    <button type="submit" class="edit-btn" id="edit-email-btn" data-field="email" title="Edit Email">
                         <img height="24" src="{{ asset('images/edit_square.svg') }}" alt="Edit Info">
                     </button>
                 </div>
@@ -39,8 +39,8 @@
             <div class="input-cont">
                 @error('password') <span style="color:crimson">{{ $message }}</span> @enderror
                 <div class="edit-input">
-                    <input type="password" name="password" id="password" placeholder="Leave blank to keep password" disabled>
-                    <button type="submit" class="edit-btn" id="edit-password-btn">
+                    <input type="password" name="password" id="password" placeholder="Leave blank to keep password" readonly>
+                    <button type="submit" class="edit-btn" id="edit-password-btn" data-field="password" title="Edit Password">
                         <img height="24" src="{{ asset('images/edit_square.svg') }}" alt="Edit Info">
                     </button>
                 </div>
@@ -48,7 +48,7 @@
             </div>
 
             <div class="input-cont">
-                <input type="password" name="password_confirmation" id="password_confirmation" disabled>
+                <input type="password" name="password_confirmation" id="password_confirmation" readonly>
                 <label for="password_confirmation">Confirm Password</label>
             </div>
 
@@ -59,3 +59,38 @@
         <p style="color: crimson">Unauthorised access</p>
     @endif
 @endsection
+
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const inputs = document.querySelectorAll('.edit-input input');
+        const editBtns = document.querySelectorAll('.edit-btn');
+        const submitBtn = document.getElementById('submit-btn');
+
+        // Toggles the edit state of the input fields
+        editBtns.forEach(btn => {
+            btn.addEventListener('click', function(event) {
+                event.preventDefault();
+                const field = this.getAttribute('data-field');
+                toggleEdit(field);
+            });
+        });
+
+        // Enable the submit button when any input field is modified
+        inputs.forEach(input => {
+            input.addEventListener('input', function() {
+                submitBtn.removeAttribute('disabled');
+            });
+        });
+
+        function toggleEdit(field) {
+            const input = document.getElementById(field);
+            input.toggleAttribute('readonly');
+
+            if (field === 'password') {
+                const confirmInput = document.getElementById('password_confirmation');
+                confirmInput.toggleAttribute('readonly');
+            }
+        }
+    });
+</script>
