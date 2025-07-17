caeadca6b10acc72edeeb81b641369042894b22e
SimonMck789 <S9406133@student.rmit.edu.au>
2025-07-18 00:17:54 +1000

    Refactor bootsrap/app.php to configure trustproxies middleware to trust all proxies - heroku deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,23 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(
+    basePath: dirname(__DIR__)
+)
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(
+            at: '*',  // Trust all proxies (Heroku’s router IPs are dynamic)
+            headers: Request::HEADER_X_FORWARDED_AWS_ELB // Recognize Heroku's forwarded headers
+        );
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+    ->create();
